e2fe5e866108de3b4bda6cc756b341119a73abf7
ZachyDev <mosetizachary001@gmail.com>
2022-06-16 13:29:06 +0300

    response object and setting up header information

--- Clients-Servers/server.js
+++ Clients-Servers/server.js
@@ -3,6 +3,15 @@ const http = require('http');
 
 const server = http.createServer((req,res) => {
     console.log('Request made');
+    console.log(req.url, req.method);
+
+    // set header content type
+    res.setHeader('Content-Type', 'text/html');
+
+    res.write('<head><link rel="stylesheet" href="style.css"></head>')
+    res.write('<p>Hello ZachyDev</p>');
+    res.write('<p>Hello again ZachyDev</p>');
+    res.end();
 })
 
 // listen to a port
