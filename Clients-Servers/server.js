14cb2020d3107e255457be574ac9d611e6834e69
ZachyDev <mosetizachary001@gmail.com>
2022-06-19 19:32:58 +0300

    Understanding (200,404) status codes

--- Clients-Servers/server.js
+++ Clients-Servers/server.js
@@ -4,10 +4,7 @@ const http = require('http');
 const fs = require('fs');
 
 const server = http.createServer((req,res) => {
-    console.log('Request has been made');
-    // console.log(req.url);
-    // console.log(req.method)
-    
+
     // setting header
     res.setHeader('Content-Type', 'text/html');
         let path =  '../views/';
@@ -16,14 +13,17 @@ const server = http.createServer((req,res) => {
         switch(req.url) {
             case '/':
             path += 'index.html'
+            res.statusCode = 200;
             break;
 
             case '/about':
             path += 'about.html'
+            res.statusCode = 200;
             break;
 
             default:
             path += '404.html'
+            res.statusCode = 404;
             break;
         }
 
