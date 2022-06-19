2e4d73af1160d95733bf165594067b71efd7d493
ZachyDev <mosetizachary001@gmail.com>
2022-06-19 19:23:05 +0300

    Client and Server communication

--- Clients-Servers/server.js
+++ Clients-Servers/server.js
@@ -1,46 +1,48 @@
-// import https module
-const fs = require('fs');
+// import http module
 const http = require('http');
+// fs module
+const fs = require('fs');
 
 const server = http.createServer((req,res) => {
-    console.log('Request made');
-    console.log(req.url, req.method);
-
-    // set header content type
-    res.setHeader('Content-Type', 'text/html');
-
-    let path = '../views/';
-    switch(req.url) {
-        case '/':
-        path += 'index.html';
-        break;
-
-        case '/about':
-        path += 'about.html'
-        break;
-
-        default:
-        path += '404.html';
-        break;
-    }
-
-    // send html file
-    fs.readFile(path, (err,data) => {
-        if (err) {
-            console.log(err);
-            res.end();
-        }
-        else {
-            res.write(data);
-            res.end();
-        }
-    });
+    console.log('Request has been made');
+    // console.log(req.url);
+    // console.log(req.method)
     
+    // setting header
+    res.setHeader('Content-Type', 'text/html');
+        let path =  '../views/';
+
+        // switch
+        switch(req.url) {
+            case '/':
+            path += 'index.html'
+            break;
+
+            case '/about':
+            path += 'about.html'
+            break;
+
+            default:
+            path += '404.html'
+            break;
+        }
+
+        fs.readFile(path, (err,data) => {
+            
+            if (err) {
+                console.log(err);
+            }
+            else {
+                res.write(data);
+                res.end();
+            }
+        });
+
+   
 })
 
-// listen to a port
-let port = 4000;
+let port = 3000;
 
 server.listen(port, 'localhost', () => {
-    console.log(`Listening for request on port ${ port }`);
+    console.log(`App listening on port ${ port }`);
 })
